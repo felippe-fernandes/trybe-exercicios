8b14fef6aa7be5953cd61bf0717806181046ae05
Felippe Fernandes <felippe1000@hotmail.com>
2021-11-08 15:06:51 -0300

    dia 4.1

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,18 @@
+let resultadoAprovacao = "reprovada";
+
+switch (resultadoAprovacao) {
+  case "aprovada":
+    console.log("aprovada");
+    break;
+
+  case "lista":
+    console.log("lista");
+    break;
+
+  case "reprovada":
+    console.log("reprovada");
+    break;
+
+  default:
+    console.log("nao se aplica");
+}
