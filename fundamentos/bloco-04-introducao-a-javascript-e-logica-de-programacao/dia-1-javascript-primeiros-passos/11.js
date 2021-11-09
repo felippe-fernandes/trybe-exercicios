f4c09a14bd44d9352a38db6afc00467326bc2d0d
Felippe Fernandes <felippe1000@hotmail.com>
2021-11-09 14:49:43 -0300

    exercicio 11 js

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/11.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/11.js
@@ -1,27 +1,35 @@
-const salarioBruto = 3354.12;
-let salarioBase;
-let salarioLiquido;
+const salarioBruto = 3000;
+let aliquotaINSS;
+let aliquotaIR;
+
 if (salarioBruto <= 1556.94) {
-  salarioBase = salarioBruto - (salarioBruto * 8) / 100;
-} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
-  salarioBase = salarioBruto - (salarioBruto * 9) / 100;
-} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
-  salarioBase = salarioBruto - (salarioBruto * 11) / 100;
+  aliquotaINSS = salarioBruto * 0.08;
+} else if (salarioBruto <= 2594.92) {
+  aliquotaINSS = salarioBruto * 0.09;
+} else if (salarioBruto <= 5189.82) {
+  aliquotaINSS = salarioBruto * 0.11;
 } else {
-  salarioBase = salarioBruto - (salarioBruto + 570.88);
-}
-return
-
-if (salarioBase <= 1903, 98) {
-  salarioLiquido = salarioBase;
-} else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
-  salarioLiquido = salarioBase - (salarioBase * 0.075) - 142.8;
-} else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
-  salarioLiquido = salarioBase - (salarioBase * 0.15) - 354.8;
-} else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
-  salarioLiquido = salarioBase - (salarioBase * 0.225) - 636.13;
-} else {
-  salarioLiquido = salarioBase - (salarioBase * 0.275) - 869.36;
+  aliquotaINSS = 570.88;
 }
 
+const salarioBase = salarioBruto - aliquotaINSS;
+
+if ((salarioBase <= 1903, 98)) {
+  aliquotaIR = 0;
+} else if (salarioBase <= 2826.65) {
+  aliquotaIR = salarioBase * 0.075 - 142.8;
+} else if (salarioBase <= 3751.05) {
+  aliquotaIR = salarioBase * 0.15 - 354.8;
+} else if (salarioBase <= 4664.68) {
+  aliquotaIR = salarioBase * 0.225 - 636.13;
+} else {
+  aliquotaIR = salarioBase * 0.275 - 869.36;
+}
+
+const salarioLiquido = salarioBase - aliquotaIR;
+
+console.log(salarioBruto);
+console.log(aliquotaINSS);
+console.log(salarioBase);
+console.log(aliquotaIR);
 console.log(salarioLiquido);
